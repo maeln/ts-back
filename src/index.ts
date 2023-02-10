a488a2ffd8eb2443fa5b370852e0e18e3e57631c
Maël Naccache Tüfekçi <contact@maeln.com>
2023-02-10 15:19:06 +0000

    yep

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
-import express from "express";
+import express, { Request } from "express";
+import https from "https";
+import { Response } from "node-fetch";
 
 const app = express();
 const port = 3005;
 
+type PokemonName = string;
+type Pokemon = { name: string, url: string }
+
+const getPokemonNameList: (callback: ((pokemons: PokemonName[]) => any)) => void = (callback) => {
+  https.get("https://pokeapi.co/api/v2/pokemon/", (res) => {
+    if (res.statusCode !== 200) {
+      console.error("request interupted");
+      res.resume();
+      return;
+    }
+
+    let data = "";
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    res.on('close', () => {
+      console.log("Finished");
+      const dataJson = JSON.parse(data)
+      const res: Pokemon[] = dataJson.results;
+      const nameList: PokemonName[] = []
+      res?.forEach(obj => {
+        nameList.push(obj.name);
+      });
+      callback(nameList);
+    });
+  })
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World 2 1 4 7!");
+  getPokemonNameList((pokemonNames) => {
+    res.send(pokemonNames)
+  })
 });
 
 app.listen(port, () => {
